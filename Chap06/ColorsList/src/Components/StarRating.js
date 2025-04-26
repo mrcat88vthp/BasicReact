0857759a19e7696100af49980ac2dc0efecf701d
mrcat88vthp <mrcat88vthp@gmail.com>
2025-04-26 23:46:20 +0700

    20250426

--- Chap06/ColorsList/src/Components/StarRating.js
+++ Chap06/ColorsList/src/Components/StarRating.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { FaStar } from "react-icons/fa";
+import Star from "./Star";
+import {createArray}  from "./lib";
 
-const createArray = (length) => [...Array(length)];
-
-const Star = ({ selected = false, onSelect = f => f }) => (
-    <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
-);
-
-export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
+export default function StarRating({ totalStars = 5, selectedStars = 0, onRate = f => f }) {
     return (
         <>
             {createArray(totalStars).map((_arrElement, index) => (
                 <Star
                     key={index}
                     selected={selectedStars > index}
+                    onSelect={() => onRate(index + 1)}
                 />
             ))}
             <p>{selectedStars} of {totalStars} stars</p>
